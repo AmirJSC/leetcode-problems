bf0cc1070e51170abb6589418fe30201db48bda2
Amir Jann Castaneda <amir@zap.com.ph>
2022-12-11 02:09:41 +0800

    Reverse a linked list

---
+++ reverse-linked-list.ts
@@ -0,0 +1,21 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+// Input: [1,2,3,4,5], Output: [5,4,3,2,1]
+function reverseList(head: ListNode | null): ListNode | null {
+  let current = head;
+  let previous = null;
+  while(current) {
+    let next = current.next; 
+    current.next = previous; 
+    previous = current;
+    current = next; 
+  }
+  return previous;
+}

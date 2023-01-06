b74dfafb0a1bd454e75202d66cb7a85e99d87f22
Amir Jann Castaneda <amir@zap.com.ph>
2023-01-06 22:50:44 +0800

    Create solution for first bad version

---
+++ easy/first-bad-version.ts
@@ -0,0 +1,17 @@
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    let start = 0;
+    let end = n - 1;
+
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+
+      if (isBadVersion(mid)) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  };
+};

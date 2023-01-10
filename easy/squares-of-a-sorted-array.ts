ae68806db1f48e6ded7a963c4d182b30a8d56f75
Amir Jann Castaneda <amir@zap.com.ph>
2023-01-10 22:53:09 +0800

    Solve squares of a sorted array

---
+++ easy/squares-of-a-sorted-array.ts
@@ -0,0 +1,18 @@
+function sortedSquares(nums: number[]): number[] {
+  let array = [];
+  let l = 0;
+  let r = nums.length - 1;
+  let p = r;
+
+  while (l <= r) {
+    if (nums[r] ** 2 > nums[l] ** 2) {
+      array[p] = nums[r] ** 2;
+      r--;
+    } else {
+      array[p] = nums[l] ** 2;
+      l++;
+    }
+    p--;
+  }
+  return array;
+}
